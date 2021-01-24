3e532d7519ff8737948e6d4275a95ee54facef3c
Akash Umakantha <akash.umakantha@vanderbilt.edu>
2021-01-24 14:02:31 -0500

    update example

--- data_example.m
+++ data_example.m
@@ -12,7 +12,7 @@ f=figure; pos=get(f,'Position'); set(f,'Position',pos.*[1 1 2 1]);
 subplot(1,2,1); hold on;
 plot(ccaResults.canonVarsX(1,:),ccaResults.canonVarsY(1,:),'.')
 plot(suffStats.Zx_mean(1,:),suffStats.Zy_mean(1,:),'.')
-xlabel('X dim 1 scores');, ylabel('Y dim 1 scores')
+xlabel('X dim 1 scores'); ylabel('Y dim 1 scores')
 xlim([-4 4]),ylim([-4 4])
 title(sprintf('r_1 = %.3f',ccaResults.canonCorr(1)))
 legend('CCA','prob CCA');
@@ -20,7 +20,7 @@ legend('CCA','prob CCA');
 subplot(1,2,2); hold on;
 plot(ccaResults.canonVarsX(2,:),ccaResults.canonVarsY(2,:),'.')
 plot(suffStats.Zx_mean(2,:),suffStats.Zy_mean(2,:),'.')
-xlabel('X dim 2 scores');, ylabel('Y dim 2 scores')
+xlabel('X dim 2 scores'); ylabel('Y dim 2 scores')
 xlim([-4 4]),ylim([-4 4])
 title(sprintf('r_2 = %.3f',ccaResults.canonCorr(2)))
 legend('CCA','prob CCA');
